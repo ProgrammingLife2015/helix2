bf937fbe82deb4abc270b41a831ce3e7bb6b5edf
Tom Brouws <tombrouws@icloud.com>
2015-04-24 00:57:18 +0200

    Set-up Maven project, move files to appropriate location

---
+++ app/src/main/java/tudelft/ti2806/pl3/App.java
@@ -0,0 +1,13 @@
+package tudelft.ti2806.pl3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
