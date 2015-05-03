f349a0746fa6628b536e04719f5a530482d5456d
Sam Smulders <sam_smulders@live.nl>
2015-05-03 17:28:47 +0200

    Foundation for GA for y-axis ordering of graph.

---
+++ src/main/java/tudelft/ti2806/pl3/data/graph/positioning/EdgeTarget.java
@@ -0,0 +1,65 @@
+package tudelft.ti2806.pl3.data.graph.positioning;
+
+import tudelft.ti2806.pl3.data.graph.Edge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EdgeTarget implements Target {
+	protected final Edge edge;
+	protected List<Target> intersectCandidates = new ArrayList<Target>();
+	
+	public EdgeTarget(Edge edge) {
+		this.edge = edge;
+	}
+	
+	/**
+	 * Select all candidates for intersection.
+	 * 
+	 * @param targets
+	 *            all targets form the graph
+	 */
+	public void selectCandidates(List<Target> targets) {
+		long start = getStart();
+		long end = getEnd();
+		for (Target target : targets) {
+			if (target.getEnd() > start && target.getStart() < end
+					&& target != this) {
+				intersectCandidates.add(target);
+			}
+		}
+	}
+	
+	/**
+	 * Counts the number of intersects with this order configuration.
+	 * 
+	 * @param order
+	 *            the order of nodes, with each node id as index
+	 * @return the count of intersects detected
+	 */
+	public int calculateIntersects(int[] order) {
+		int count = 0;
+		Line line = getLine(order);
+		for (Target target : intersectCandidates) {
+			if (target.getLine(order).intersect(line)) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	@Override
+	public Line getLine(int[] order) {
+		return new Line(this.getStart(),
+				order[this.edge.getFrom().getNodeId()], this.getEnd(),
+				order[this.edge.getTo().getNodeId()]);
+	}
+	
+	public long getStart() {
+		return edge.getFrom().getXEnd();
+	}
+	
+	public long getEnd() {
+		return edge.getTo().getXaxisStart();
+	}
+}
---
+++ src/test/java/tudelft/ti2806/pl3/data/graph/positioning/EdgeTargetTest.java
@@ -0,0 +1,69 @@
+package tudelft.ti2806.pl3.data.graph.positioning;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import tudelft.ti2806.pl3.data.graph.Edge;
+import tudelft.ti2806.pl3.data.graph.GraphData;
+import tudelft.ti2806.pl3.data.graph.Node;
+import tudelft.ti2806.pl3.data.graph.SingleNode;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EdgeTargetTest {
+	private File nodesFile = new File("data/7TestNodes.node.graph");
+	private File edgesFile = new File("data/7TestNodes.edge.graph");
+	
+	@Test
+	public void calculateIntersectsTest() throws FileNotFoundException {
+		// Load data
+		GraphData gd = GraphData.parseGraph(nodesFile, edgesFile);
+		List<Edge> testEdges = new ArrayList<Edge>();
+		Edge edge34 = gd.getEdge(3, 4);
+		testEdges.add(edge34);
+		Edge edge35 = gd.getEdge(3, 5);
+		testEdges.add(edge35);
+		Edge edge36 = gd.getEdge(3, 6);
+		testEdges.add(edge36);
+		Edge edge67 = gd.getEdge(6, 7);
+		testEdges.add(edge67);
+		
+		// Create target list
+		List<Target> targets = new ArrayList<Target>();
+		for (Node node : gd.getNodes()) {
+			SingleNode snode = (SingleNode) node;
+			snode.calculateStartX();
+			targets.add(new NodeTarget(snode));
+		}
+		Map<Edge, EdgeTarget> testTargets = new HashMap<Edge, EdgeTarget>();
+		for (Edge edge : gd.getEdges()) {
+			if (testEdges.contains(edge)) {
+				EdgeTarget testTarget = new EdgeTarget(edge);
+				testTargets.put(edge, testTarget);
+				targets.add(testTarget);
+			} else {
+				targets.add(new EdgeTarget(edge));
+			}
+		}
+		
+		// Select candidates
+		for (EdgeTarget target : testTargets.values()) {
+			target.selectCandidates(targets);
+		}
+		assertTrue(testTargets.get(edge34).intersectCandidates.size() == 4);
+		assertTrue(testTargets.get(edge35).intersectCandidates.size() == 4);
+		assertTrue(testTargets.get(edge36).intersectCandidates.size() == 4);
+		assertTrue(testTargets.get(edge67).intersectCandidates.size() == 2);
+		int[] order = new int[] { -1, 1, 2, 3, 4, 5, 6, 7 };
+		assertTrue(testTargets.get(edge34).calculateIntersects(order) == 2);
+		assertTrue(testTargets.get(edge35).calculateIntersects(order) == 1);
+		assertTrue(testTargets.get(edge36).calculateIntersects(order) == 0);
+		assertTrue(testTargets.get(edge67).calculateIntersects(order) == 0);
+	}
+}
