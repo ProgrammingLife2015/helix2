aadef9112b55d999cb08a7ca2792d131b6ebf452
Sam Smulders <sam_smulders@live.nl>
2015-05-03 12:09:26 +0200

    Added location attributes and methods to nodes

--- src/main/java/tudelft/ti2806/pl3/graph/GraphModel.java
+++ src/main/java/tudelft/ti2806/pl3/graph/GraphModel.java
@@ -245,7 +245,8 @@ public class GraphModel {
 		boolean found = true;
 		for (Edge edge : edges) {
 			if (lastEdge != null
-					&& edge.getTo().getNodeId() == lastEdge.getTo().getNodeId()) {
+					&& edge.getTo().getNodeId()
+					== lastEdge.getTo().getNodeId()) {
 				found = true;
 			} else {
 				if (found == false) {
