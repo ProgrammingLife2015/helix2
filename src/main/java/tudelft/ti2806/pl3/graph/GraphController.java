f02200714993769aa079585609448edce894b4ce
Boris Mattijssen <borismattijssen@gmail.com>
2015-05-06 15:33:55 +0200

    Checkstyle fixes

--- src/main/java/tudelft/ti2806/pl3/graph/GraphController.java
+++ src/main/java/tudelft/ti2806/pl3/graph/GraphController.java
@@ -4,37 +4,36 @@ import org.graphstream.graph.Graph;
 import tudelft.ti2806.pl3.Controller;
 import tudelft.ti2806.pl3.data.GraphParser;
 
-import java.awt.*;
+import java.awt.Component;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 /**
  * Controls the graph view.
- *
  * Created by Boris Mattijssen on 30-04-15.
  */
 public class GraphController implements Controller {
 
-    private GraphView view;
+	private GraphView view;
 
-    /**
-     * Initialize view and pass the graph to it
-     */
-    public GraphController() {
-        view = new GraphView();
+	/**
+	 * Initialize view and pass the graph to it.
+	 */
+	public GraphController() {
+		view = new GraphView();
 
-        try {
-            Graph graph = GraphParser.parseGraph("hello", new File(
-                    "data/10_strains_graph/simple_graph.node.graph"), new File(
-                    "data/10_strains_graph/simple_graph.edge.graph"));
-            view.setGraph(graph);
-        } catch (FileNotFoundException e) {
-            view.showFileNotFound();
-        }
-    }
+		try {
+			Graph graph = GraphParser.parseGraph("hello", new File(
+					"data/10_strains_graph/simple_graph.node.graph"), new File(
+					"data/10_strains_graph/simple_graph.edge.graph"));
+			view.setGraph(graph);
+		} catch (FileNotFoundException e) {
+			view.showFileNotFound();
+		}
+	}
 
-    @Override
-    public Component getView() {
-        return view;
-    }
+	@Override
+	public Component getView() {
+		return view;
+	}
 }
--- src/main/java/tudelft/ti2806/pl3/graph/GraphView.java
+++ src/main/java/tudelft/ti2806/pl3/graph/GraphView.java
@@ -3,52 +3,53 @@ package tudelft.ti2806.pl3.graph;
 import org.graphstream.graph.Graph;
 import org.graphstream.ui.swingViewer.Viewer;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 
 /**
- * Render the sequence graph
- *
+ * Render the sequence graph.
  * Created by Boris Mattijssen on 30-04-15.
  */
 public class GraphView extends JPanel {
 
-    private Graph graph;
+	private Graph graph;
 
-    /**
-     * Initialize graph view, give it a BorderLayout
-     */
-    public GraphView() {
-        this.setLayout(new BorderLayout());
-    }
+	/**
+	 * Initialize graph view, give it a BorderLayout.
+	 */
+	public GraphView() {
+		this.setLayout(new BorderLayout());
+	}
 
-    /**
-     * Set the graph and render it into the view
-     * @param graph the graph from the graphstream library
-     */
-    public void setGraph(Graph graph) {
-        this.graph = graph;
-        renderGraph();
-    }
+	/**
+	 * Set the graph and render it into the view.
+	 * @param graph the graph from the graphstream library
+	 */
+	public void setGraph(Graph graph) {
+		this.graph = graph;
+		renderGraph();
+	}
 
-    /**
-     * Render the sequence graph and add it to this panel
-     */
-    public void renderGraph() {
-        removeAll();
-        graph.addAttribute("ui.antialias");
+	/**
+	 * Render the sequence graph and add it to this panel.
+	 */
+	public void renderGraph() {
+		removeAll();
+		graph.addAttribute("ui.antialias");
 
-        Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
-        viewer.enableAutoLayout();
+		Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
+		viewer.enableAutoLayout();
 
-        this.add(viewer.addDefaultView(false));
-    }
+		this.add(viewer.addDefaultView(false));
+	}
 
-    /**
-     * When the graph file was not found, show this message instead of the graph
-     */
-    public void showFileNotFound() {
-        removeAll();
-        add(new JLabel("The graph file could not be found"));
-    }
+	/**
+	 * When the graph file was not found, show this message instead of the graph.
+	 */
+	public void showFileNotFound() {
+		removeAll();
+		add(new JLabel("The graph file could not be found"));
+	}
 }
