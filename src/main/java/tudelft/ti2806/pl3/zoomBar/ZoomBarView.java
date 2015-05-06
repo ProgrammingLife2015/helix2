19b38c076f230534a1ffb6e8dc60190be963b66c
Boris Mattijssen <borismattijssen@gmail.com>
2015-05-06 15:37:27 +0200

    View initialized

--- src/main/java/tudelft/ti2806/pl3/zoomBar/ZoomBarView.java
+++ src/main/java/tudelft/ti2806/pl3/zoomBar/ZoomBarView.java
@@ -1,9 +1,23 @@
 package tudelft.ti2806.pl3.zoomBar;
 
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
 
 /**
+ * The view for the zoom bar.
+ * The zoom bar is used to navigate through and zoom in on the graph.
  * Created by Boris Mattijssen on 06-05-15.
  */
-public class ZoomBarView extends JPanel{
+public class ZoomBarView extends JPanel {
+
+	/**
+	 * Construct a zoom bar view with a fixed height.
+	 */
+	public ZoomBarView() {
+		this.setLayout(new BorderLayout());
+		setPreferredSize(new Dimension(-1,200));
+	}
+
 }
