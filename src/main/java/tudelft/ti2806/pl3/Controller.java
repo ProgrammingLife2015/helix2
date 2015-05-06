e1e5e2cd3f664e3e35f7eec5f2226b8fa758d383
Sam Smulders <sam_smulders@live.nl>
2015-05-06 22:44:46 +0200

    Small checkstyle import fix

--- src/main/java/tudelft/ti2806/pl3/Controller.java
+++ src/main/java/tudelft/ti2806/pl3/Controller.java
@@ -1,13 +1,14 @@
 package tudelft.ti2806.pl3;
 
-import java.awt.*;
+import java.awt.Component;
 
 /**
  * Interface for all Controllers. All controllers should contain a view.
  *
+ * <p>
  * Created by Boris Mattijssen on 30-04-15.
  */
 public interface Controller {
-
-    Component getView();
+	
+	public Component getView();
 }
