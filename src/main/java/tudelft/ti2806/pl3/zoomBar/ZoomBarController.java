9ae496f4facb9dd02b1c031e307bb8eaba87cac0
Boris Mattijssen <borismattijssen@gmail.com>
2015-05-06 15:38:28 +0200

    Graph controller injected

--- src/main/java/tudelft/ti2806/pl3/zoomBar/ZoomBarController.java
+++ src/main/java/tudelft/ti2806/pl3/zoomBar/ZoomBarController.java
@@ -1,20 +1,33 @@
 package tudelft.ti2806.pl3.zoomBar;
 
 import tudelft.ti2806.pl3.Controller;
+import tudelft.ti2806.pl3.graph.GraphController;
 
-import java.awt.*;
+import java.awt.Component;
 
 /**
+ * Controller that controls the zoom bar at the bottom of the screen.
+ * The zoom bar is used to navigate through and zoom in on the graph.
  * Created by Boris Mattijssen on 06-05-15.
  */
 public class ZoomBarController implements Controller {
 
+	private GraphController graphController;
 	private ZoomBarView zoomBarView;
 
-	public ZoomBarController() {
+	/**
+	 * Construct a new controller for the zoom bar.
+	 * @param graphController instance of the graph controller to update its view
+	 */
+	public ZoomBarController(GraphController graphController) {
+		this.graphController = graphController;
 		zoomBarView = new ZoomBarView();
 	}
 
+	/**
+	 * Get the zoom bar view.
+	 * @return the view
+	 */
 	@Override
 	public Component getView() {
 		return zoomBarView;
