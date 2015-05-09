e48d654e7bf416bddf65a64f1df31e21f41aa45f
Kasper Wendel <kasperwendel@gmail.com>
2015-05-09 15:37:25 +0200

    GUI now has Nimbus Theme

--- src/main/java/tudelft/ti2806/pl3/Main.java
+++ src/main/java/tudelft/ti2806/pl3/Main.java
@@ -1,5 +1,7 @@
 package tudelft.ti2806.pl3;
 
+import javax.swing.UIManager;
+
 /**
  * Main application launcher.
  * Created by Boris Mattijssen on 30-04-15.
@@ -11,6 +13,16 @@ public class Main {
 	 * @param args input arguments
 	 */
 	public static void main(String[] args) {
+		try {
+			for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (Exception e) {
+			// If Nimbus is not available, you can set the GUI to another look and feel.
+		}
 		Application app = new Application();
 		app.make();
 		app.start();
