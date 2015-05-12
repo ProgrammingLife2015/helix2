ab0b0b7c5777afceb76498619b2ca79be56d3c98
Boris Mattijssen <borismattijssen@gmail.com>
2015-05-12 11:28:20 +0200

    Set up view and controller

---
+++ src/main/java/tudelft/ti2806/pl3/sidebar/genomeFilter/GenomeFilterView.java
@@ -0,0 +1,16 @@
+package tudelft.ti2806.pl3.sidebar.genomeFilter;
+
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * Created by Boris Mattijssen on 12-05-15.
+ */
+public class GenomeFilterView extends JPanel {
+
+	public GenomeFilterView() {
+		setLayout(new BorderLayout());
+		add(new JLabel("Test"));
+	}
+}
