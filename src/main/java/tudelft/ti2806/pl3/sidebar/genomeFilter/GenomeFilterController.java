67665c2a429ea2f7aa27faa775fc7a637884e47c
Boris Mattijssen <borismattijssen@gmail.com>
2015-05-12 11:28:20 +0200

    Set up view and controller

---
+++ src/main/java/tudelft/ti2806/pl3/sidebar/genomeFilter/GenomeFilterController.java
@@ -0,0 +1,20 @@
+package tudelft.ti2806.pl3.sidebar.genomeFilter;
+
+import tudelft.ti2806.pl3.Controller;
+
+import java.awt.*;
+
+/**
+ * Created by Boris Mattijssen on 12-05-15.
+ */
+public class GenomeFilterController implements Controller {
+
+	public GenomeFilterController() {
+
+	}
+
+	@Override
+	public Component getPanel() {
+		return null;
+	}
+}
