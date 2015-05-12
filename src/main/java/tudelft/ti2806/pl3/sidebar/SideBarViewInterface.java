dc3e3895efa88b57d11d998ae46807ff13faff83
Boris Mattijssen <borismattijssen@gmail.com>
2015-05-12 21:33:08 +0200

    Added the list

---
+++ src/main/java/tudelft/ti2806/pl3/sidebar/SideBarViewInterface.java
@@ -0,0 +1,7 @@
+package tudelft.ti2806.pl3.sidebar;
+
+/**
+ * Created by Boris Mattijssen on 12-05-15.
+ */
+public interface SideBarViewInterface {
+}
--- src/main/java/tudelft/ti2806/pl3/sidebar/genomeFilter/GenomeFilterView.java
+++ src/main/java/tudelft/ti2806/pl3/sidebar/genomeFilter/GenomeFilterView.java
@@ -1,16 +1,27 @@
 package tudelft.ti2806.pl3.sidebar.genomeFilter;
 
-import java.awt.BorderLayout;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import tudelft.ti2806.pl3.sidebar.SideBarViewInterface;
+
+import java.awt.*;
+import javax.swing.*;
 
 /**
  * Created by Boris Mattijssen on 12-05-15.
  */
-public class GenomeFilterView extends JPanel {
+public class GenomeFilterView extends JPanel implements SideBarViewInterface {
 
 	public GenomeFilterView() {
-		setLayout(new BorderLayout());
-		add(new JLabel("Test"));
+		String[] data = new String[2];
+		data[0] = "Hoi";
+		data[1] = "Doei";
+		JList<String> list = new JList(data); //data has type Object[]
+		list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		list.setLayoutOrientation(JList.VERTICAL);
+		list.setVisibleRowCount(-1);
+
+		JScrollPane listScroller = new JScrollPane(list);
+		listScroller.setPreferredSize(new Dimension(250, 80));
+
+		add(listScroller);
 	}
 }
