367f041433cacd00e8fd47d59a16129ae8cae265
Boris Mattijssen <borismattijssen@gmail.com>
2015-05-13 12:06:04 +0200

    Changed filter interface

--- src/main/java/tudelft/ti2806/pl3/data/filter/OccurFilter.java
+++ src/main/java/tudelft/ti2806/pl3/data/filter/OccurFilter.java
@@ -2,21 +2,18 @@ package tudelft.ti2806.pl3.data.filter;
 
 import tudelft.ti2806.pl3.data.graph.Node;
 
+import java.util.Collection;
 import java.util.List;
 
-public class OccurFilter extends Filter<Node> {
+public class OccurFilter implements Filter<Node> {
 	protected final int occurCount;
 	
 	public OccurFilter(int occurCount) {
 		this.occurCount = occurCount;
 	}
-	
+
 	@Override
-	public void calculateFilter(List<Node> list) {
-		for (Node node : list) {
-			if (node.getSource().length < this.occurCount) {
-				filter.add(node);
-			}
-		}
+	public void filter(Collection<Node> list) {
+
 	}
 }
