ce781f9377e3a2c0a3687db69da3d5b8f4da58dc
Boris Mattijssen <borismattijssen@gmail.com>
2015-05-13 12:08:38 +0200

    Implemented real filter

--- src/main/java/tudelft/ti2806/pl3/data/filter/GenomeFilter.java
+++ src/main/java/tudelft/ti2806/pl3/data/filter/GenomeFilter.java
@@ -3,21 +3,25 @@ package tudelft.ti2806.pl3.data.filter;
 import tudelft.ti2806.pl3.data.Genome;
 import tudelft.ti2806.pl3.data.graph.Node;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
-public class GenomeFilter extends Filter<Node> {
-	protected final Genome genome;
+public class GenomeFilter implements Filter<Node> {
+	protected final List<String> genomes;
 	
-	public GenomeFilter(Genome genome) {
-		this.genome = genome;
+	public GenomeFilter(List<String> genomes) {
+		this.genomes = genomes;
 	}
-	
+
 	@Override
-	public void calculateFilter(List<Node> list) {
-		for (Node node : list) {
-			for (Genome source : node.getSource()) {
-				if (source.equals(genome)) {
-					filter.add(node);
+	public void filter(Collection<Node> nodes) {
+		List<Node> newList = new ArrayList<>();
+		for (Node node : nodes) {
+			for(int j = 0; j< node.getSource().length; j++) {
+				if(genomes.contains(node.getSource()[j].getIdentifier())) {
+					newList.add(node);
+					break;
 				}
 			}
 		}
