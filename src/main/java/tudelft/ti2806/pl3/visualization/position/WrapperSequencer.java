1097e2f3fd2869b637ec172ccea667e6d9752f47
Sam Smulders <sam_smulders@live.nl>
2015-05-15 17:50:57 +0200

    WrapperSequencer javadoc update

--- src/main/java/tudelft/ti2806/pl3/visualization/position/WrapperSequencer.java
+++ src/main/java/tudelft/ti2806/pl3/visualization/position/WrapperSequencer.java
@@ -1,7 +1,10 @@
 package tudelft.ti2806.pl3.visualization.position;
 
+import tudelft.ti2806.pl3.visualization.position.wrapper.CombineWrapper;
+import tudelft.ti2806.pl3.visualization.position.wrapper.HorizontalWrapper;
 import tudelft.ti2806.pl3.visualization.position.wrapper.NodeWrapper;
 import tudelft.ti2806.pl3.visualization.position.wrapper.SpaceWrapper;
+import tudelft.ti2806.pl3.visualization.position.wrapper.VerticalWrapper;
 
 /**
  * A {@link WrapperSequencer} is used to calculate the order of each
@@ -12,8 +15,58 @@ import tudelft.ti2806.pl3.visualization.position.wrapper.SpaceWrapper;
  */
 public interface WrapperSequencer extends WrapperOperation {
 	/**
-	 * The first and the last element of the nodes within SpaceWrapper should be
-	 * left on their original locations.
+	 * Calculate the order for {@link HorizontalWrapper}.
+	 * 
+	 * <p>
+	 * Change the order of the nodes of the {@link SpaceWrapper} its list.
+	 * 
+	 * <p>
+	 * The default method just calls the {@link #calculate(NodeWrapper)} for
+	 * each {@link NodeWrapper} within this {@link CombineWrapper}.
+	 * 
+	 * @param wrapper
+	 *            the node to perform the operation on
+	 */
+	default void calculate(HorizontalWrapper wrapper) {
+		for (NodeWrapper node : wrapper.getNodeList()) {
+			calculate(node);
+		}
+	}
+	
+	/**
+	 * Calculate the order for {@link VerticalWrapper}.
+	 * 
+	 * <p>
+	 * Change the order of the nodes of the {@link SpaceWrapper} its list.
+	 * 
+	 * <p>
+	 * The default method just calls the {@link #calculate(NodeWrapper)} for
+	 * each {@link NodeWrapper} within this {@link CombineWrapper}.
+	 * 
+	 * @param wrapper
+	 *            the node to perform the operation on
+	 */
+	default void calculate(VerticalWrapper wrapper) {
+		for (NodeWrapper node : wrapper.getNodeList()) {
+			calculate(node);
+		}
+	}
+	
+	/**
+	 * Calculate the order for {@link SpaceWrapper}.
+	 * 
+	 * <p>
+	 * Change the order of incoming or outgoing connections for each node within
+	 * the {@link SpaceWrapper} its list, to change the order within the
+	 * wrapper. Don't change the order of the nodes within
+	 * {@link CombineWrapper}.
+	 * 
+	 * <p>
+	 * The default method just calls the {@link #calculate(NodeWrapper)} for
+	 * each {@link NodeWrapper} within this {@link CombineWrapper}.
+	 * 
+	 * @param wrapper
+	 *            the node to perform the operation on
 	 */
 	default void calculate(SpaceWrapper wrapper) {
 		for (NodeWrapper node : wrapper.getNodeList()) {
