1729d3c2903c9f2ced749664dbdb51c8a3dd95b2
Mathieu Post <mathieupost@gmail.com>
2015-05-18 11:07:50 +0200

    Skelleton CollapseOperation

---
+++ src/main/java/tudelft/ti2806/pl3/visualization/wrapper/operation/collapse/CollapseOperation.java
@@ -0,0 +1,27 @@
+package tudelft.ti2806.pl3.visualization.wrapper.operation.collapse;
+
+import tudelft.ti2806.pl3.visualization.wrapper.HorizontalWrapper;
+import tudelft.ti2806.pl3.visualization.wrapper.NodeWrapper;
+import tudelft.ti2806.pl3.visualization.wrapper.SpaceWrapper;
+import tudelft.ti2806.pl3.visualization.wrapper.VerticalWrapper;
+import tudelft.ti2806.pl3.visualization.wrapper.operation.WrapperOperation;
+
+/**
+ * Created by Mathieu Post on 18-5-15.
+ */
+public class CollapseOperation implements WrapperOperation {
+	@Override
+	public void calculate(HorizontalWrapper wrapper, NodeWrapper container) {
+
+	}
+
+	@Override
+	public void calculate(SpaceWrapper wrapper, NodeWrapper container) {
+
+	}
+
+	@Override
+	public void calculate(VerticalWrapper wrapper, NodeWrapper container) {
+
+	}
+}
