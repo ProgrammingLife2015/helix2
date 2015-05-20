d7b513cb62436ff47f8d3a772091cc9497e2cbbc
Mathieu Post <mathieupost@gmail.com>
2015-05-20 14:24:54 +0200

    Create tests for GraphData

--- src/test/java/tudelft/ti2806/pl3/data/graph/GraphDataTest.java
+++ src/test/java/tudelft/ti2806/pl3/data/graph/GraphDataTest.java
@@ -3,6 +3,11 @@ package tudelft.ti2806.pl3.data.graph;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import tudelft.ti2806.pl3.data.Genome;
+import tudelft.ti2806.pl3.data.graph.node.DataNodeInterface;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -12,12 +17,19 @@ import static org.mockito.Mockito.*;
  */
 public class GraphDataTest {
 	GraphData graphData;
+	AbstractGraphData abstractGraphData;
+	List<DataNodeInterface> nodeList = new ArrayList<>();
+	List<Edge> edgeList = new ArrayList<>();
+	List<Genome> genomeList = new ArrayList<>();
 
 	@Before
 	public void setUp() throws Exception {
-		AbstractGraphData graphData = mock(AbstractGraphData.class);
-//		when(graphData.getNodeListClone()).thenReturn()
-//		graphData = new GraphData()
+		abstractGraphData = mock(AbstractGraphData.class);
+		when(abstractGraphData.getNodeListClone()).thenReturn(nodeList);
+		when(abstractGraphData.getEdgeListClone()).thenReturn(edgeList);
+		when(abstractGraphData.getGenomeClone()).thenReturn(genomeList);
+		when(abstractGraphData.getLongestNodePath()).thenReturn(99);
+		graphData = new GraphData(abstractGraphData);
 	}
 
 	@After
@@ -27,26 +39,26 @@ public class GraphDataTest {
 
 	@Test
 	public void testGetNodes() throws Exception {
-
+		assertEquals(nodeList, graphData.getNodes());
 	}
 
 	@Test
 	public void testGetEdges() throws Exception {
-
+		assertEquals(edgeList, graphData.getEdges());
 	}
 
 	@Test
 	public void testGetGenomes() throws Exception {
-
+		assertEquals(genomeList, graphData.getGenomes());
 	}
 
 	@Test
 	public void testGetOrigin() throws Exception {
-
+		assertEquals(abstractGraphData, graphData.getOrigin());
 	}
 
 	@Test
 	public void testGetLongestNodePath() throws Exception {
-
+		assertEquals(99, graphData.getLongestNodePath());
 	}
 }
