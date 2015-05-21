8430a0ee485ccdb61c8437ff47c5e2bf0b8a79cb
Boris Mattijssen <borismattijssen@gmail.com>
2015-05-21 14:22:10 +0200

    Moved interest calculations to FilteredGraphModel

--- src/main/java/tudelft/ti2806/pl3/visualization/ZoomedGraphModel.java
+++ src/main/java/tudelft/ti2806/pl3/visualization/ZoomedGraphModel.java
@@ -1,12 +1,8 @@
 package tudelft.ti2806.pl3.visualization;
 
-import tudelft.ti2806.pl3.visualization.wrapper.CombineWrapper;
 import tudelft.ti2806.pl3.visualization.wrapper.DataNodeWrapper;
 import tudelft.ti2806.pl3.visualization.wrapper.NodeWrapper;
 import tudelft.ti2806.pl3.visualization.wrapper.operation.collapse.CollapseOnInterest;
-import tudelft.ti2806.pl3.visualization.wrapper.operation.interest.CalculateAddMaxOfWrapped;
-import tudelft.ti2806.pl3.visualization.wrapper.operation.interest.CalculateSizeInterest;
-import tudelft.ti2806.pl3.visualization.wrapper.operation.interest.CalculateWrapPressureInterest;
 import tudelft.ti2806.pl3.visualization.wrapper.operation.unwrap.Unwrap;
 
 import java.util.List;
@@ -23,18 +19,12 @@ public class ZoomedGraphModel extends Observable implements Observer {
 	private List<DataNodeWrapper> dataNodeWrapperList;
 
 	private double zoomLevel = 10;
-	private final int pressureMultiplier = 10;
 
-	private CalculateWrapPressureInterest pressureInterest;
-	private CalculateAddMaxOfWrapped addMaxOfWrapped;
-	private CalculateSizeInterest sizeInterest;
-	//private CalculateGroupInterest groupInterest;
+
 
 	public ZoomedGraphModel(FilteredGraphModel filteredGraphModel) {
 		this.filteredGraphModel = filteredGraphModel;
-		pressureInterest = new CalculateWrapPressureInterest(pressureMultiplier);
-		addMaxOfWrapped = new CalculateAddMaxOfWrapped();
-		sizeInterest = new CalculateSizeInterest();
+
 	}
 
 	public List<DataNodeWrapper> getDataNodeWrapperList() {
@@ -50,15 +40,12 @@ public class ZoomedGraphModel extends Observable implements Observer {
 	}
 
 	public void produceDataNodeWrapperList() {
-		pressureInterest.calculate(collapsedNode, null);
-		addMaxOfWrapped.calculate(collapsedNode, null);
-		sizeInterest.calculate(collapsedNode, null);
 		CollapseOnInterest collapseOnInterest = new CollapseOnInterest(100);
 		collapseOnInterest.calculate(collapsedNode, null);
 
 		Unwrap unwrap = new Unwrap(collapsedNode);
 		dataNodeWrapperList = unwrap.getDataNodeWrappers();
-		// TODO: calc x-pos
+
 		setChanged();
 		notifyObservers();
 	}
