8758c8ab4075aec1c442e6b62b2e414c7a8d5243
Kasper Wendel <kasperwendel@gmail.com>
2015-05-27 16:56:19 +0200

    PMD fix + loading screen start-up

---
+++ src/main/java/tudelft/ti2806/pl3/loading/LoadingScreen.java
@@ -0,0 +1,18 @@
+package tudelft.ti2806.pl3.loading;
+
+import tudelft.ti2806.pl3.LoadingObservable;
+import tudelft.ti2806.pl3.LoadingObserver;
+
+import javax.swing.JDialog;
+
+/**
+ * Created by Kasper on 27-5-2015.
+ */
+public class LoadingScreen extends JDialog implements LoadingObserver {
+
+
+	@Override
+	public void update(LoadingObservable loadingObservable, Object arguments) {
+
+	}
+}
