6c8a0283773dace3092ffa9f87332af1f1d43e56
Boris Mattijssen <borismattijssen@gmail.com>
2015-05-28 17:25:59 +0200

    Tried a new approach for fixing empty edges

--- src/main/java/tudelft/ti2806/pl3/util/EdgeUtil.java
+++ src/main/java/tudelft/ti2806/pl3/util/EdgeUtil.java
@@ -1,15 +1,19 @@
 package tudelft.ti2806.pl3.util;
 
+import tudelft.ti2806.pl3.data.Genome;
 import tudelft.ti2806.pl3.data.graph.DataNode;
 import tudelft.ti2806.pl3.data.graph.Edge;
 import tudelft.ti2806.pl3.data.wrapper.WrappedGraphData;
 import tudelft.ti2806.pl3.data.wrapper.Wrapper;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * An utility class to find and/or remove dead edges from a {@link GraphData}
@@ -25,30 +29,56 @@ public class EdgeUtil {
 		for (Wrapper wrapper : wrappedGraphData.getPositionedNodes()) {
 			boolean allEqual = true;
 			if (wrapper.getOutgoing().size() <= 1) {
-				allEqual = false;
+				break;
 			}
 
-			Map<Integer, Wrapper> nodeCountMapping = new HashMap<>(wrapper.getOutgoing().size());
-			for (Wrapper outgoing : wrapper.getOutgoing()) {
-				nodeCountMapping.put(outgoing.getPreviousNodesCount(), outgoing);
-				if (!new HashableCollection<>(wrapper.getGenome())
-						.equals(new HashableCollection<>(outgoing.getGenome()))) {
-					allEqual = false;
-					break;
+//			Map<Genome, Boolean> genomesMap = new HashMap<>();
+//			for (Genome genome : wrapper.getGenome()) {
+//				genomesMap.put(genome, false);
+//			}
+			List<Genome> genomes = new ArrayList<>(wrapper.getGenome());
+			List<Wrapper> outgoingList = new ArrayList<>(wrapper.getOutgoing());
+			outgoingList.sort(Comparator.<Wrapper>naturalOrder());
+			List<Wrapper> removeList = new ArrayList<>();
+			for (Wrapper outgoing : outgoingList) {
+//				for (Genome genome : outgoing.getGenome()) {
+//					if(genomesMap.get(genome) == false) {
+//						genomesMap.put(genome, true);
+//					} else if(genomesMap.get(genome) == true) {
+//						removeList.add(outgoing);
+//					}
+//				}
+				if(genomes.size() == 0) {
+					removeList.add(outgoing);
+				} else {
+					genomes.removeAll(outgoing.getGenome());
 				}
 			}
-			if(allEqual == true) {
-				List<Integer> list = new ArrayList<>(nodeCountMapping.keySet());
-				Collections.sort(list);
-				List<Wrapper> removeList = new ArrayList<>();
-				for (Wrapper outgoing : wrapper.getOutgoing()) {
-					if(outgoing != nodeCountMapping.get(list.get(0))) {
-						removeList.add(outgoing);
-						outgoing.getIncoming().remove(wrapper);
-					}
-				}
-				wrapper.getOutgoing().removeAll(removeList);
+			for (Wrapper remove : removeList) {
+				remove.getIncoming().remove(wrapper);
 			}
+			wrapper.getOutgoing().removeAll(removeList);
+//			Map<Integer, Wrapper> nodeCountMapping = new HashMap<>(wrapper.getOutgoing().size());
+//			for (Wrapper outgoing : wrapper.getOutgoing()) {
+//				nodeCountMapping.put(outgoing.getPreviousNodesCount(), outgoing);
+//				if (!new HashableCollection<>(wrapper.getGenome())
+//						.equals(new HashableCollection<>(outgoing.getGenome()))) {
+//					allEqual = false;
+//					break;
+//				}
+//			}
+//			if(allEqual == true) {
+//				List<Integer> list = new ArrayList<>(nodeCountMapping.keySet());
+//				Collections.sort(list);
+//				List<Wrapper> removeList = new ArrayList<>();
+//				for (Wrapper outgoing : wrapper.getOutgoing()) {
+//					if(outgoing != nodeCountMapping.get(list.get(0))) {
+//						removeList.add(outgoing);
+//						outgoing.getIncoming().remove(wrapper);
+//					}
+//				}
+//				wrapper.getOutgoing().removeAll(removeList);
+//			}
 		}
 	}
 	
