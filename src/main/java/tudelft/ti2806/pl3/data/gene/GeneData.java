fb8315eae44e9a85a7c6a541dbac6f93d5cb4738
Tom Brouws <tombrouws@icloud.com>
2015-05-30 15:50:05 +0200

    Replace .* import by single imports

--- src/main/java/tudelft/ti2806/pl3/data/gene/GeneData.java
+++ src/main/java/tudelft/ti2806/pl3/data/gene/GeneData.java
@@ -1,6 +1,9 @@
 package tudelft.ti2806.pl3.data.gene;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
