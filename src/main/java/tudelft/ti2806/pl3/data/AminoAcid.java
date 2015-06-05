a4177023d44bb2da38fed56cf29879151cb50330
Mathieu Post <mathieupost@gmail.com>
2015-06-05 12:52:00 +0200

    Fix long line

--- src/main/java/tudelft/ti2806/pl3/data/AminoAcid.java
+++ src/main/java/tudelft/ti2806/pl3/data/AminoAcid.java
@@ -36,7 +36,8 @@ public enum AminoAcid {
 	private static AminoAcid[] fillTranslationTable() {
 		AminoAcid[] table = new AminoAcid[CODONS];
 		try {
-			Scanner scanner = new Scanner(AminoAcid.class.getClassLoader().getResourceAsStream(TRANSLATION_TABLE_FILE), "UTF-8");
+			Scanner scanner = new Scanner(AminoAcid.class.getClassLoader()
+					.getResourceAsStream(TRANSLATION_TABLE_FILE), "UTF-8");
 			for (int i = 0; i < CODONS; i++) {
 				String[] data = scanner.nextLine().split(" ");
 				if (!data[0].startsWith("#")) {
