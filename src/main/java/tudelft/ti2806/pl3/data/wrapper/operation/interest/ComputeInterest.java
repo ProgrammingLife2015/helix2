c4384893a681b9c0fda7f96fe8de623e2f965bc2
Sam Smulders <sam_smulders@live.nl>
2015-06-09 11:29:51 +0200

    Javadoc for ComputeInterest

--- src/main/java/tudelft/ti2806/pl3/data/wrapper/operation/interest/ComputeInterest.java
+++ src/main/java/tudelft/ti2806/pl3/data/wrapper/operation/interest/ComputeInterest.java
@@ -3,6 +3,12 @@ package tudelft.ti2806.pl3.data.wrapper.operation.interest;
 import tudelft.ti2806.pl3.data.wrapper.Wrapper;
 
 public class ComputeInterest {
+	/**
+	 * Computes the interest values on the graph.
+	 * 
+	 * @param wrapper
+	 *            the wrapper
+	 */
 	public void compute(Wrapper wrapper) {
 		new CalculateWrapPressureInterest().calculate(wrapper, null);
 		new CalculateMultiplyWithContainerInterest().calculate(wrapper, null);
