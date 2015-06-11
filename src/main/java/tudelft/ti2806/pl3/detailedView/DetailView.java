188d09b4220a638d6f4b7dddb2f0834bb21a3937
Mathieu Post <mathieupost@gmail.com>
2015-06-11 10:37:43 +0200

    Add labels to DetailView and override setBounds to place the view better.

--- src/main/java/tudelft/ti2806/pl3/detailedView/DetailView.java
+++ src/main/java/tudelft/ti2806/pl3/detailedView/DetailView.java
@@ -1,9 +1,12 @@
 package tudelft.ti2806.pl3.detailedView;
 
 import tudelft.ti2806.pl3.data.Genome;
+import tudelft.ti2806.pl3.data.label.Label;
 import tudelft.ti2806.pl3.data.wrapper.WrapperClone;
 
+import java.awt.Container;
 import java.awt.Dimension;
+import java.util.Set;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
@@ -35,12 +38,42 @@ public class DetailView extends JPanel {
 	 */
 	public void setNode(WrapperClone node, int x, int y) {
 		removeAll();
+
+		add(new JLabel("Genomes:"));
 		for (Genome genome : node.getGenome()) {
 			JLabel label = new JLabel(genome.toString());
 			add(label);
 		}
 
+		Set<Label> labels = node.getLabels();
+		if (labels.size() > 0) {
+			JLabel jLabel = new JLabel("Labels:");
+			jLabel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+			add(jLabel);
+			for (Label label : labels) {
+				add(new JLabel(label.getText()));
+			}
+		}
+
 		Dimension size = getPreferredSize();
 		setBounds(x, y, size.width, size.height);
+	}
+
+	@Override
+	public void setBounds(int x, int y, int width, int height) {
+		Container parent = getParent();
+		if (parent != null) {
+			int parentWidth = parent.getWidth();
+			int parentHeight = parent.getHeight();
+
+			width = Math.min(width, parentWidth);
+			height = Math.min(height, parentHeight);
+
+			if (x > parentWidth - width) {
+				x -= width + 30;
+			}
+			y = Math.min(y, parentHeight - height);
+		}
+		super.setBounds(x, y, width, height);
 	}
 }
