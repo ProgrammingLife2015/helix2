724ddc725c8a3cef130727f4ad800b2f0dc9cdc6
Sam Smulders <sam_smulders@live.nl>
2015-06-11 15:41:29 +0200

    Integer overload fix

--- src/main/java/tudelft/ti2806/pl3/data/wrapper/operation/interest/CalculateWrapPressureInterest.java
+++ src/main/java/tudelft/ti2806/pl3/data/wrapper/operation/interest/CalculateWrapPressureInterest.java
@@ -8,26 +8,23 @@ import tudelft.ti2806.pl3.data.wrapper.operation.WrapperOperation;
 import java.util.Collection;
 
 /**
- * Adds an high interest value when the number of genomes in nodes are equally
- * distributed over the nodes, a relative low interest if the genomes are not
- * well distributed over the nodes.
+ * Adds an high interest value when the number of genomes in nodes are equally distributed over the nodes, a relative
+ * low interest if the genomes are not well distributed over the nodes.
  * 
  * @author Sam Smulders
  */
 public class CalculateWrapPressureInterest extends WrapperOperation {
 	@Override
 	public void calculate(VerticalWrapper wrapper, Wrapper container) {
-		wrapper.addInterest(wrapper.getNodeList().stream()
-				.map(Wrapper::getGenome).map(Collection::size)
-				.reduce(1, (a, b) -> a * b));
+		wrapper.addInterest((float) wrapper.getNodeList().stream().map(Wrapper::getGenome)
+				.mapToDouble(Collection::size).reduce(1.0, (a, b) -> a * b));
 		super.calculate(wrapper, container);
 	}
 	
 	@Override
 	public void calculate(SpaceWrapper wrapper, Wrapper container) {
-		wrapper.addInterest(wrapper.getNodeList().stream()
-				.map(Wrapper::getGenome).map(Collection::size)
-				.reduce(1, (a, b) -> a * b).intValue());
+		wrapper.addInterest((float) wrapper.getNodeList().stream().map(Wrapper::getGenome)
+				.mapToDouble(Collection::size).reduce(1.0, (a, b) -> a * b));
 		super.calculate(wrapper, container);
 	}
 }
