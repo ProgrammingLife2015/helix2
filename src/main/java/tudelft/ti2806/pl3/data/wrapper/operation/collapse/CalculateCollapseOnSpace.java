f4bc67c66e9cae915ba3213aee0dd148c04c01b2
Sam Smulders <sam_smulders@live.nl>
2015-06-16 20:28:01 +0200

    Fix for bad computation of space within SpaceWrapper

--- src/main/java/tudelft/ti2806/pl3/data/wrapper/operation/collapse/CalculateCollapseOnSpace.java
+++ src/main/java/tudelft/ti2806/pl3/data/wrapper/operation/collapse/CalculateCollapseOnSpace.java
@@ -6,6 +6,7 @@ import tudelft.ti2806.pl3.data.wrapper.SpaceWrapper;
 import tudelft.ti2806.pl3.data.wrapper.VerticalWrapper;
 import tudelft.ti2806.pl3.data.wrapper.Wrapper;
 import tudelft.ti2806.pl3.data.wrapper.operation.WrapperOperation;
+import tudelft.ti2806.pl3.util.DoneDeque;
 
 import java.util.Comparator;
 
@@ -15,54 +16,72 @@ import java.util.Comparator;
  * @author Sam Smulders
  */
 public class CalculateCollapseOnSpace extends WrapperOperation {
-	@Override
-	public void calculate(HorizontalWrapper wrapper, Wrapper container) {
-		if (wrapper.canUnwrap()) {
-			super.calculate(wrapper, container);
-			wrapper.addCollapse(getSpaceLeft(wrapper));
-		}
-	}
-	
-	@Override
-	public void calculate(SpaceWrapper wrapper, Wrapper container) {
-		super.calculate(wrapper, container);
-		wrapper.addCollapse(getSpaceLeft(wrapper));
-	}
-	
-	@Override
-	public void calculate(VerticalWrapper wrapper, Wrapper container) {
-		super.calculate(wrapper, container);
-		wrapper.addCollapse(Float.MAX_VALUE);
-	}
-	
-	/**
-	 * Computes if there is enough space for a wrapper to unfold it.
-	 * 
-	 * @param wrapper
-	 *            the wrapper of which to determine if there is enough space to unfold it
-	 * @return a value of the average space between nodes.
-	 */
-	float getSpaceLeft(CombineWrapper wrapper) {
-		return getMinDistance(wrapper.getFirst(), wrapper.getLast());
-	}
-	
-	private float getMinDistance(Wrapper first, Wrapper last) {
-		float min = Float.MAX_VALUE;
-		for (Wrapper wrapper : first.getOutgoing()) {
-			if (wrapper == last) {
-				min = Math.min(min, wrapper.getX() - first.getX());
-				continue;
-			}
-			min = Math.min(min, wrapper.getX() - first.getX());
-			min = Math.min(min, getMinDistance(wrapper, last));
-		}
-		return min;
-	}
-	
-	public class XComparator implements Comparator<Wrapper> {
-		@Override
-		public int compare(Wrapper w1, Wrapper w2) {
-			return (int) Math.signum(w1.getX() - w2.getX());
-		}
-	}
+    @Override
+    public void calculate(HorizontalWrapper wrapper, Wrapper container) {
+        if (wrapper.canUnwrap()) {
+            super.calculate(wrapper, container);
+            wrapper.addCollapse(getMinSpaceLeft(wrapper));
+        }
+    }
+    
+    @Override
+    public void calculate(SpaceWrapper wrapper, Wrapper container) {
+        super.calculate(wrapper, container);
+        wrapper.addCollapse(getAvgSpaceLeft(wrapper));
+    }
+    
+    @Override
+    public void calculate(VerticalWrapper wrapper, Wrapper container) {
+        super.calculate(wrapper, container);
+        wrapper.addCollapse(Float.MAX_VALUE);
+    }
+    
+    /**
+     * Computes if there is enough space for a wrapper to unfold it.
+     * 
+     * @param wrapper
+     *            the wrapper of which to determine if there is enough space to unfold it
+     * @return a value of the average space between nodes.
+     */
+    float getMinSpaceLeft(CombineWrapper wrapper) {
+        return getMinDistance(wrapper.getFirst(), wrapper.getLast());
+    }
+    
+    private float getAvgSpaceLeft(SpaceWrapper wrapper) {
+        float avg = 0;
+        DoneDeque<Wrapper> que = new DoneDeque<>(wrapper.getNodeList().size());
+        que.add(wrapper.getFirst());
+        int count = 0;
+        while (!que.isEmpty()) {
+            Wrapper next = que.poll();
+            for (Wrapper out : next.getOutgoing()) {
+                avg += out.getX() - next.getX();
+                if (out != wrapper.getLast()) {
+                    que.add(out);
+                }
+                count++;
+            }
+        }
+        return avg / count;
+    }
+    
+    private float getMinDistance(Wrapper first, Wrapper last) {
+        float min = Float.MAX_VALUE;
+        for (Wrapper wrapper : first.getOutgoing()) {
+            if (wrapper == last) {
+                min = Math.min(min, wrapper.getX() - first.getX());
+                continue;
+            }
+            min = Math.min(min, wrapper.getX() - first.getX());
+            min = Math.min(min, getMinDistance(wrapper, last));
+        }
+        return min;
+    }
+    
+    public class XComparator implements Comparator<Wrapper> {
+        @Override
+        public int compare(Wrapper w1, Wrapper w2) {
+            return (int) Math.signum(w1.getX() - w2.getX());
+        }
+    }
 }
