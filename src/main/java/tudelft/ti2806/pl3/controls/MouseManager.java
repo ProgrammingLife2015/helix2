f66647961e69c1f4cfe9b76611556dff88e146c8
Mathieu Post <mathieupost@gmail.com>
2015-06-16 17:25:13 +0200

    Fix too long line

--- src/main/java/tudelft/ti2806/pl3/controls/MouseManager.java
+++ src/main/java/tudelft/ti2806/pl3/controls/MouseManager.java
@@ -76,7 +76,8 @@ public class MouseManager extends DefaultMouseManager {
 	}
 
 	private WrapperClone getNodeAtPosition(int x, int y) {
-		ArrayList<GraphicElement> graphicElements = view.allNodesOrSpritesIn(x - MARGIN, y - MARGIN, x + MARGIN, y + MARGIN);
+		ArrayList<GraphicElement> graphicElements =
+				view.allNodesOrSpritesIn(x - MARGIN, y - MARGIN, x + MARGIN, y + MARGIN);
 		if (graphicElements.size() == 0) {
 			return null;
 		} else {
