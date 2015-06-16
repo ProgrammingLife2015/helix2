642882f0ef0e01a88bc2158f873e98d0290ddac3
Tom Brouws <tombrouws@icloud.com>
2015-06-16 13:19:11 +0200

    Add empty parser test

---
+++ src/test/java/tudelft/ti2806/pl3/data/meta/MetaParserTest.java
@@ -0,0 +1,14 @@
+package tudelft.ti2806.pl3.data.meta;
+
+import org.junit.Test;
+
+/**
+ * Created by tombrouws on 16/06/15.
+ */
+public class MetaParserTest {
+
+    @Test
+    public void parserTest() {
+
+    }
+}
