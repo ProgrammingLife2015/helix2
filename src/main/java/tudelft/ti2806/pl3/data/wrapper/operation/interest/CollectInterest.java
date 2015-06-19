0e151b8579b144109e25b4e88268e8eb4f223976
Sam Smulders <sam_smulders@live.nl>
2015-06-19 15:35:21 +0200

    Removed unused classes

--- src/main/java/tudelft/ti2806/pl3/data/wrapper/operation/interest/CollectInterest.java
+++ src/main/java/tudelft/ti2806/pl3/data/wrapper/operation/interest/CollectInterest.java
@@ -1,4 +1,4 @@
-package tudelft.ti2806.pl3.util;
+package tudelft.ti2806.pl3.data.wrapper.operation.interest;
 
 import tudelft.ti2806.pl3.data.wrapper.DataNodeWrapper;
 import tudelft.ti2806.pl3.data.wrapper.Wrapper;
@@ -13,32 +13,38 @@ import java.util.List;
 public class CollectInterest {
 	private final float[] interest;
 	private final int domain;
-	private float maxInterest = Float.MIN_VALUE;
-
+	private float maxInterest = -Float.MAX_VALUE;
+	
 	public CollectInterest(int domain) {
 		this.interest = new float[domain];
 		this.domain = domain;
 	}
-
+	
 	public float[] getInterest() {
 		return this.interest;
 	}
-
+	
 	/**
 	 * Collects interest of {@link DataNodeWrapper}'s and projects it on an array with a given size.
 	 *
 	 * @param wrappers
-	 * 		the list of wrappers
+	 *            the list of wrappers
 	 */
 	public void calculate(List<Wrapper> wrappers) {
 		double width = wrappers.stream().mapToDouble(Wrapper::getX).max().getAsDouble();
 		for (Wrapper wrapper : wrappers) {
 			int index = (int) Math.min(this.domain - 1, (wrapper.getX() / width) * this.domain);
 			this.interest[index] += wrapper.getInterest();
-			this.maxInterest = Math.max(this.maxInterest, this.interest[index]);
+//			System.out.println(wrapper.getInterest());
+		}
+		for (int i = 0; i < this.interest.length; i++) {
+//			System.out.print(this.interest[i]+"->");
+			this.interest[i] = (float) Math.sqrt(this.interest[i]);
+//			System.out.println(this.interest[i]);
+			this.maxInterest = Math.max(this.maxInterest, this.interest[i]);
 		}
 	}
-
+	
 	public float getMaxInterest() {
 		return this.maxInterest;
 	}
