30b2e13c0568844cf0d8ec5f9967b7f3836d9c3d
Sam Smulders <sam_smulders@live.nl>
2015-06-19 12:07:47 +0200

    Added commas for thousands

--- src/main/java/tudelft/ti2806/pl3/visualization/GraphView.java
+++ src/main/java/tudelft/ti2806/pl3/visualization/GraphView.java
@@ -22,29 +22,27 @@ import tudelft.ti2806.pl3.util.observers.LoadingObserver;
 import java.awt.Component;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Observable;
 import java.util.Observer;
 
 /**
- * The GraphView is responsible for adding the nodes and edges to the graph,
- * keeping the nodes and edges on the right positions and applying the right
- * style to the graph.
+ * The GraphView is responsible for adding the nodes and edges to the graph, keeping the nodes and edges on the right
+ * positions and applying the right style to the graph.
  *
  * @author Sam Smulders
  *
  */
-public class GraphView
-		implements Observer, tudelft.ti2806.pl3.View, ViewInterface, LoadingObservable {
+public class GraphView implements Observer, tudelft.ti2806.pl3.View, ViewInterface, LoadingObservable {
 	private static final float EDGE_THICKNESS_SCALE = 10f;
-
-    /**
+	
+	/**
 	 * The zoomLevel used to draw the graph.<br>
-	 * A zoom level of 1.0 shows the graph 1:1, so that every base pair should
-	 * be readable, each with  pixels to draw its
-	 * value as text. A zoom level of 2.0 shows the graph with each base pair
-	 * using the half this size.
+	 * A zoom level of 1.0 shows the graph 1:1, so that every base pair should be readable, each with pixels to draw its
+	 * value as text. A zoom level of 2.0 shows the graph with each base pair using the half this size.
 	 */
 	private double zoomLevel = 1.0;
 	
@@ -60,16 +58,16 @@ public class GraphView
 	private ArrayList<LoadingObserver> loadingObservers = new ArrayList<>();
 	private MouseManager mouseManager;
 	private ArrayList<GraphLoadedListener> graphLoadedListeners = new ArrayList<>();
-
+	
 	private float offsetToCenter = -1;
 	private boolean zoomCenterSet = false;
 	private ZoomedGraphModel zoomedGraphModel;
-
+	
 	/**
 	 * Construct a GraphView.
 	 *
 	 * @param zoomedGraphModel
-	 * 		The zoomed graph model
+	 *            The zoomed graph model
 	 */
 	public GraphView(ZoomedGraphModel zoomedGraphModel) {
 		this.zoomedGraphModel = zoomedGraphModel;
@@ -78,9 +76,8 @@ public class GraphView
 	}
 	
 	/**
-	 * Generates a {@link Viewer} for the graph with the given {@code zoomLevel}
-	 * . A new Viewer should be constructed every time the graphData or
-	 * zoomLevel updates.
+	 * Generates a {@link Viewer} for the graph with the given {@code zoomLevel} . A new Viewer should be constructed
+	 * every time the graphData or zoomLevel updates.
 	 */
 	private void generateViewer() {
 		viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_SWING_THREAD);
@@ -97,22 +94,22 @@ public class GraphView
 			}
 		});
 	}
-
+	
 	/**
-	 * Remove the default keys from the GraphStream library, since we use our own.
-	 * There is no other way than this to do it.
+	 * Remove the default keys from the GraphStream library, since we use our own. There is no other way than this to do
+	 * it.
 	 */
 	public void removeDefaultKeys() {
-		DefaultShortcutManager listener = (DefaultShortcutManager)this.getPanel().getKeyListeners()[0];
+		DefaultShortcutManager listener = (DefaultShortcutManager) this.getPanel().getKeyListeners()[0];
 		listener.release();
 	}
-
+	
 	/**
 	 * Sets the graph its drawing properties.
 	 */
 	private void setGraphPropertys() {
 		final String stylesheet = "stylesheet.css";
-
+		
 		graph.addAttribute("ui.quality");
 		graph.addAttribute("ui.antialias");
 		graph.addAttribute("ui.stylesheet", "url('" + stylesheet + "')");
@@ -128,19 +125,19 @@ public class GraphView
 		graph.clear();
 		setGraphPropertys();
 		final double someSize = panel.getBounds().height
-				/ (panel.getBounds().width * zoomLevel / zoomedGraphModel
-				.getWrappedCollapsedNode().getWidth())
-				/ zoomedGraphModel.getWrappedCollapsedNode().getGenome().size();
+			/ (panel.getBounds().width * zoomLevel / zoomedGraphModel.getWrappedCollapsedNode().getWidth())
+			/ zoomedGraphModel.getWrappedCollapsedNode().getGenome().size();
 		graphData.forEach(node -> {
 				Node graphNode = graph.addNode(Integer.toString(node.getId()));
 				double y = node.getY() * someSize;
 				graphNode.setAttribute("xy", node.getX(), y);
-				graphNode.addAttribute("ui.class", node.getOriginalNode().getClass()
-							.getSimpleName());
-				graphNode.addAttribute("ui.label", node.getOriginalNode().getBasePairCount());
+				graphNode.addAttribute("ui.class", node.getOriginalNode().getClass().getSimpleName());
+				graphNode.addAttribute("ui.label",
+					NumberFormat.getNumberInstance(Locale.US)
+					.format(node.getOriginalNode().getBasePairCount()));
 				graphNode.setAttribute("node", node);
 			});
-
+		
 		for (WrapperClone node : graphData) {
 			int i = 0;
 			for (Wrapper to : node.getOutgoing()) {
@@ -150,7 +147,7 @@ public class GraphView
 				i++;
 			}
 		}
-
+		
 		notifyLoadingObservers(false);
 		return graph;
 	}
@@ -163,21 +160,20 @@ public class GraphView
 	 * @param from
 	 *            the node where the edge begins
 	 * @param to
-	 * 		the node where the edge ends
+	 *            the node where the edge ends
 	 * @param i
-	 * 		the index of the edge in the list of outgoing edges
+	 *            the index of the edge in the list of outgoing edges
 	 */
 	@SuppressWarnings("PMD.UnusedPrivateMethod")
 	private void addNormalEdge(Graph graph, Wrapper from, Wrapper to, int i) throws EdgeZeroWeightException {
-		Edge edge = graph.addEdge(from.getId() + "-" + to.getId(),
-				Integer.toString(from.getId()), Integer.toString(to.getId()), true);
+		Edge edge = graph.addEdge(from.getId() + "-" + to.getId(), Integer.toString(from.getId()),
+				Integer.toString(to.getId()), true);
 		int weight = from.getOutgoingWeight().get(i);
 		float percent = ((float) weight) / ((float) zoomedGraphModel.getGenomesCount());
-
+		
 		if (weight == 0) {
 			edge.addAttribute("ui.label", "fix me!");
-			throw new EdgeZeroWeightException(
-					"The weight of the edge from " + from + " to " + to + " cannot be 0.");
+			throw new EdgeZeroWeightException("The weight of the edge from " + from + " to " + to + " cannot be 0.");
 		} else {
 			edge.addAttribute("ui.style", "size: " + (percent * EDGE_THICKNESS_SCALE) + "px;");
 		}
@@ -187,7 +183,7 @@ public class GraphView
 	public Component getPanel() {
 		return panel;
 	}
-
+	
 	@Override
 	public void update(Observable o, Object arg) {
 		if (o == zoomedGraphModel) {
@@ -196,14 +192,14 @@ public class GraphView
 			try {
 				generateGraph();
 			} catch (EdgeZeroWeightException e) {
-				//TODO: show popup?
+				// TODO: show popup?
 				e.printStackTrace();
 			}
 			zoom();
 			centerGraph();
 		}
 	}
-
+	
 	private void centerGraph() {
 		if (zoomCenterSet == false) {
 			setZoomCenter(0);
@@ -212,19 +208,19 @@ public class GraphView
 			zoomCenterSet = true;
 		}
 	}
-
+	
 	private void zoom() {
 		viewer.getDefaultView().getCamera().setViewPercent(1 / zoomLevel);
 	}
-
+	
 	public float getZoomCenter() {
 		return (float) viewer.getDefaultView().getCamera().getViewCenter().x;
 	}
-
+	
 	public double getViewPercent() {
 		return viewer.getDefaultView().getCamera().getViewPercent();
 	}
-
+	
 	/**
 	 * Set the offset of the center of the graph to the left edge of the screen.
 	 */
@@ -233,30 +229,30 @@ public class GraphView
 				.transformPxToGu(0, ScreenSize.getInstance().getHeight() / 2d);
 		offsetToCenter = (float) point3.x * -1;
 	}
-
+	
 	public float getOffsetToCenter() {
 		return offsetToCenter;
 	}
-
+	
 	/**
 	 * Moves the view to the given position on the x axis.
 	 *
 	 * @param zoomCenter
-	 * 		the new center of view
+	 *            the new center of view
 	 */
 	public void setZoomCenter(float zoomCenter) {
 		viewer.getDefaultView().getCamera().setViewCenter(zoomCenter, 0, 0);
 	}
-
+	
 	/**
-	 * Centers the graph on a specific node. It passes a {@link DataNode} and then looks in the list of currently
-	 * drawn {@link WrapperClone}s, which one contains this {@link DataNode} and then sets the zoom center on this
+	 * Centers the graph on a specific node. It passes a {@link DataNode} and then looks in the list of currently drawn
+	 * {@link WrapperClone}s, which one contains this {@link DataNode} and then sets the zoom center on this
 	 * {@link WrapperClone}.
 	 *
 	 * @param node
-	 * 		The {@link DataNode} to move the view to
+	 *            The {@link DataNode} to move the view to
 	 * @throws NodeNotFoundException
-	 * 		Thrown when the node cannot be found in all {@link WrapperClone}s
+	 *             Thrown when the node cannot be found in all {@link WrapperClone}s
 	 */
 	public void centerOnNode(DataNode node) throws NodeNotFoundException {
 		float x = -1;
@@ -273,48 +269,47 @@ public class GraphView
 					+ " you are looking for cannot be found in the current graph.");
 		}
 	}
-
+	
 	public double getGraphDimension() {
 		return viewer.getDefaultView().getCamera().getGraphDimension();
 	}
-
+	
 	@Override
 	public void addLoadingObserver(LoadingObserver loadingObservable) {
 		loadingObservers.add(loadingObservable);
 	}
-
-
+	
 	@Override
 	public void addLoadingObserversList(ArrayList<LoadingObserver> loadingObservers) {
 		for (LoadingObserver loadingObserver : loadingObservers) {
 			addLoadingObserver(loadingObserver);
 		}
 	}
-
+	
 	@Override
 	public void deleteLoadingObserver(LoadingObserver loadingObservable) {
 		loadingObservers.remove(loadingObservable);
 	}
-
+	
 	@Override
 	public void notifyLoadingObservers(Object arguments) {
 		for (LoadingObserver loadingObserver : loadingObservers) {
 			loadingObserver.update(this, arguments);
 		}
 	}
-
+	
 	public void addGraphLoadedListener(GraphLoadedListener listener) {
 		graphLoadedListeners.add(listener);
 	}
-
+	
 	public void removeGraphLoadedListener(GraphLoadedListener listener) {
 		graphLoadedListeners.remove(listener);
 	}
-
+	
 	public void notifyGraphLoadedListeners() {
 		graphLoadedListeners.forEach(GraphLoadedListener::graphLoaded);
 	}
-
+	
 	public void removeDetailView() {
 		mouseManager.removeDetailView();
 	}
