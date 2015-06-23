54f1fb3bc7535ef044292409f5591c40db3e91ab
Sam Smulders <sam_smulders@live.nl>
2015-06-23 11:57:15 +0200

    Fix duplicate code

--- src/main/java/tudelft/ti2806/pl3/util/CollectionUtil.java
+++ src/main/java/tudelft/ti2806/pl3/util/CollectionUtil.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by Boris Mattijssen on 17-06-15.
@@ -25,7 +26,7 @@ public class CollectionUtil {
 	 * @return the intersection between the two collections
 	 */
 	public static <T> Collection<T> intersection(Collection<T> collection1, Collection<T> collection2) {
-		return collection1.stream().filter(collection2::contains).collect(Collectors.toList());
+		return filter(collection1, collection2).collect(Collectors.toList());
 	}
 
 	/**
@@ -40,6 +41,21 @@ public class CollectionUtil {
 	 * @return the intersection between the two collections
 	 */
 	public static <T> Set<T> intersectionListSet(List<T> collection1, Set<T> collection2) {
-		return collection1.stream().filter(collection2::contains).collect(Collectors.toSet());
+		return filter(collection1, collection2).collect(Collectors.toSet());
+	}
+
+	/**
+	 * Calculates the intersection between two collections.
+	 * 
+	 * @param collection1
+	 * 		collection 1
+	 * @param collection2
+	 * 		collection 2
+	 * @param <T>
+	 * 		type of both collections
+	 * @return the intersection between the two collections as a stream
+	 */
+	public static <T> Stream<T> filter(Collection<T> collection1, Collection<T> collection2) {
+		return collection1.stream().filter(collection2::contains);
 	}
 }
